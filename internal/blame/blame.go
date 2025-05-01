5832e67064bd4b69e2c1bf907fed68915f1650af
Vasilis Papavasileiou <vasilisp@users.noreply.github.com>
2025-05-01 02:01:23 +0000

    Add more detailed system prompt

--- internal/blame/blame.go
+++ internal/blame/blame.go
@@ -8,6 +8,7 @@ import (
 	"github.com/vasilisp/lingograph"
 	"github.com/vasilisp/lingograph/extra"
 	"github.com/vasilisp/lingograph/openai"
+	"github.com/vasilisp/semblame/internal/data"
 	"github.com/vasilisp/semblame/internal/git"
 	"github.com/vasilisp/semblame/internal/shared"
 )
@@ -39,7 +40,7 @@ func Blame(ctx context.Context, repoPath string, matches []shared.Match, query s
 	messages = append(messages, lingograph.UserPrompt(query, false))
 
 	client := openai.NewClient(openai.APIKeyFromEnv())
-	actor := openai.NewActor(client, openai.GPT4oMini, "You are a helpful assistant that implements a fancier version of git blame. You will receive a list of code changes and a commit message. You will then return a list of the code changes that are most likely to be the cause of the commits followed by the user query. Please answer the query based on the code changes. Please focus on the most relevant commits and don't feel obliged to mention every single one.", nil)
+	actor := openai.NewActor(client, openai.GPT4oMini, data.SystemPrompt, nil)
 
 	messages = append(messages, actor.Pipeline(extra.Echoln(os.Stdout, ""), false, 3))
 
